e539e1eb2e1897d9e76c2ea72fad956e920b6485
Enterpryz <enterpryz1@gmail.com>
2020-02-17 18:39:10 -0600

    Added text component, removed unneeded sprites. Text 101 V. 1.0.1

---
+++ Assets/SpaceGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start() {
+        textComponent.text = ("Texty texto, I'm a textatious text meant to be tested. I have no soul but that's ok.");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
