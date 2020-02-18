5f0e91788bc177add578862bcdd06599897da417
Enterpryz <enterpryz1@gmail.com>
2020-02-17 21:01:13 -0600

    Create a Public Method

--- Assets/SpaceGame.cs
+++ Assets/SpaceGame.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 public class SpaceGame : MonoBehaviour {
 
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
 
     // Start is called before the first frame update
     void Start() {
-        textComponent.text = ("Texty texto, I'm a textatious text meant to be tested. I have no soul but that's ok.");
+        state = startingState;
+        textComponent.text = state.GetStateStory();
         
     }
 
